7843d3bf9efff427ecf4ad9447cd15fb318e32fe
mochimosh101 <mochimosh03@gmail.com>
2026-06-22 23:25:57 -0700

    Unify portfolio branding on mobile

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const includePartials = async () => {
-  const includeVersion = "20260623e";
+  const includeVersion = "20260623f";
   const includeElements = Array.from(document.querySelectorAll("x-include"));
 
   await Promise.all(includeElements.map(async (element) => {

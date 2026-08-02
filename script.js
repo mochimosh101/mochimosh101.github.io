f0b2cd24ced7f0e4766f7983c8610ddb9bf1fed4
mochimosh101 <mochimosh03@gmail.com>
2026-08-02 01:36:01 -0700

    Bump portfolio responsive cache key

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const includePartials = async () => {
-  const includeVersion = "20260802c";
+  const includeVersion = "20260802d";
   const includeElements = Array.from(document.querySelectorAll("x-include"));
 
   await Promise.all(includeElements.map(async (element) => {
